aa0c70af29daa755b6f0cd8023814a49ba441bf6
Panda <73842511+PandaCatPlayz@users.noreply.github.com>
2022-05-18 18:56:23 -0700

    renaming
    from bonkity to tronkity uk?

--- randomCFiles/source.c
+++ randomCFiles/source.c
@@ -1,7 +1,7 @@
 // Sensor and motor initilizers
 #pragma config(Sensor, in1, lightsensor, sensorReflection)
 #pragma config(Sensor, dgtl1, encoder,  sensorQuadEncoder)
-#pragma config(Sensor, dgtl3, red, sensorLEDtoVCC)
+#pragma config(Sensor, dgtl12, red, sensorLEDtoVCC)
 #pragma config(Sensor, dgtl4, green, sensorLEDtoVCC)
 #pragma config(Motor, port3, flashlight, tmotorVexFlashlight, openLoop, reversed)
 #pragma config(Motor, port2, motor, tmotorVex393_MC29, openLoop)
@@ -10,14 +10,14 @@
 task main()
 {
     // Initialize variables
-    int waitTime = 2;
-    int speed = 40;
-    int maxEncode = 115;
-    int minEncode = 0;
+    int waitTime = 4;
+    int speed = 45;
+    int maxEncode = 119;
+    int minEncode = -115;
 
     // Initializes flashlight and off LED
     turnFlashlightOn(port3,100);
-    turnLEDOn(dgtl3);
+    turnLEDOn(dgtl12);
 
     // Wait
     wait(waitTime);
@@ -25,9 +25,9 @@ task main()
     // Essentially while(true) constantly runs
     while(1==1) {
         // Detects when the light sensor is NOT BELOW OR EQUAL TO 52
-        if(SensorValue[lightsensor]!>=52) {
+        if(SensorValue[in1]>=100) {
             // Toggle LEDs
-            turnLEDOff(dgtl3);
+            turnLEDOff(dgtl5);
             turnLEDOn(dgtl4);
 
             // Wait
@@ -38,16 +38,15 @@ task main()
             untilEncoderCounts(maxEncode,dgtl1);
             stopMotor(port2);
 
-            // Wait
-            wait(waitTime);
+            wait(waitTime/4);
 
             // Run motor until encoder hits bottom
-            startMotor(port2,speed);
+            startMotor(port2,40);
             untilEncoderCounts(minEncode,dgtl1);
             stopMotor(port2);
 
             // Toggle LEDs
-            turnLEDOn(dgtl3);
+            turnLEDOn(dgtl12);
             turnLEDOff(dgtl4);
         }
     }
