66790983fc5667c252e79b22fe506cb245c237ae
Panda <PandaCatPlayz@gmail.com>
2022-09-13 11:46:04 -0700

    Update source.c

---
+++ random-c/source.c
@@ -0,0 +1,13 @@
+#pragma config(Sensor, in1, LineFollower, sensorLineFollower)
+#pragma config(Sensor, in2, potentiometer, sensorPotentiometer)
+#pragma config(Sensor, in3, lightsensor, sensorReflection)
+#pragma config(Sensor, dgtl1, limitswitch, sensorTouch)
+#pragma config(Sensor, dgtl2, bumpswitch, sensorTouch)
+#pragma config(Sensor, dgtl3, quad, sensorQuadEncoder)
+#pragma config(Sensor, dgtl5, sonar, sensorSONAR_inch)
+#pragma config(Sensor, dgtl12, green, sensorLEDtoVCC)
+#pragma config(Motor,  port1, FlashLight, tmotorVexFlashlight, openLoop, reversed)
+#pragma config(Motor,  port2, rightmotor, tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3, leftmotor, tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port9, Servomotor, tmotorServoStandard, openLoop)
+
