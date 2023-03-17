71e657c824aac0a05bc871a63754a541d5c4999f
sakuradev <PandaCatPlayz@gmail.com>
2023-03-17 10:53:30 -0700

    stupdi js

--- TexturePackHub/assets/js/packForm.js
+++ TexturePackHub/assets/js/packForm.js
@@ -1,9 +1,75 @@
 function sendToExcell() {
-    var contact = document.getElementById("contactSelect")
-    var username = document.getElementById("inputUsername")
-    var name = document.getElementById("inputName")
-    var artist = document.getElementById("artistSelect")
-    var version = document.getElementById("versionSelect")
-    var blocks = document.getElementById("blocksCheck")
-    var items = document.getElementById("itemsCheck")
+    var contact = document.getElementById("contactSelect");
+    var username = document.getElementById("inputUsername");
+    var name = document.getElementById("inputName");
+    var artist = document.getElementById("artistSelect");
+    var version = document.getElementById("versionSelect");
+    var blocks = document.getElementById("blocksCheck");
+    var items = document.getElementById("itemsCheck");
+    var particles = document.getElementById("particlesCheck");
+    var gui = document.getElementById("GUICheck");
+    var animated = document.getElementById("animatedCheck");
+    var colors = document.getElementById("colorSelect");
+    var textures = document.getElementById("textureRange");
+    var complete = document.getElementById("formComplete");
+}
+
+function calculateCost() {
+    var artist = document.getElementById("artistSelect");
+    var blocks = document.getElementById("blocksCheck");
+    var items = document.getElementById("itemsCheck");
+    var particles = document.getElementById("particlesCheck");
+    var gui = document.getElementById("GUICheck");
+    var animated = document.getElementById("animatedCheck");
+    var colors = document.getElementById("colorSelect");
+    var textures = document.getElementById("textureRange");
+    var complete = document.getElementById("formComplete");
+
+    var price = 0;
+
+    if(artist.value == "sakuradev") {
+        price = 5;
+    }
+
+    if(blocks.ariaChecked) {
+        price += 1;
+    }
+
+    if(items.ariaChecked) {
+        price += 0.5;
+    }
+
+    if(particles.ariaChecked) {
+        price += 0.25;
+    }
+
+    if(gui.ariaChecked) {
+        price += 0.25;
+    }
+
+    if(animated.ariaChecked) {
+        price += 2;
+    }
+
+    if(colors.value == "One") {
+        price += 0.1;
+    }
+    else if(colors.value == "Two") {
+        price += 0.25;
+    }
+
+    if(textures.ariaValueNow > 10 && textures.ariaValueNow <= 25) {
+        price += 0.25;
+    }
+    else if(textures.ariaValueNow > 25 && textures.ariaValueNow <= 50) {
+        price += 2;
+    }
+    else if(textures.ariaValueNow > 50 && textures.ariaValueNow <= 75) {
+        price += 5;
+    }
+    else if(textures.ariaValueNow > 75) {
+        price += 10;
+    }
+
+    complete.innerHTML = "Form Completed: $" + price;
 }
