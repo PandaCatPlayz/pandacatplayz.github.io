115ee38a52802381e07c94e27e7f75104e27260a
Panda <73842511+PandaCatPlayz@users.noreply.github.com>
2023-03-16 21:51:51 -0700

    .

---
+++ TexturePackHub/assets/js/packForm.js
@@ -0,0 +1,9 @@
+function sendToExcell() {
+    var contact = document.getElementById("contactSelect")
+    var username = document.getElementById("inputUsername")
+    var name = document.getElementById("inputName")
+    var artist = document.getElementById("artistSelect")
+    var version = document.getElementById("versionSelect")
+    var blocks = document.getElementById("blocksCheck")
+    var items = document.getElementById("itemsCheck")
+}
